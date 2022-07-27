f8d2f2d316687bd2397259f4c32d0a09f7692c60
chenboling <chenboling@zly.com>
2022-07-27 14:12:38 +0800

    项目修改

---
+++ example/android/app/src/main/java/com/zly/simple_image_cropper_example/MainActivity.java
@@ -0,0 +1,6 @@
+package com.zly.simple_image_cropper_example;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- ios/Classes/SimpleImageCropperPlugin.m
+++ ios/Classes/SimpleImageCropperPlugin.m
@@ -1,15 +1,20 @@
 #import "SimpleImageCropperPlugin.h"
-#if __has_include(<simple_image_cropper/simple_image_cropper-Swift.h>)
-#import <simple_image_cropper/simple_image_cropper-Swift.h>
-#else
-// Support project import fallback if the generated compatibility header
-// is not copied when this plugin is created as a library.
-// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "simple_image_cropper-Swift.h"
-#endif
 
 @implementation SimpleImageCropperPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftSimpleImageCropperPlugin registerWithRegistrar:registrar];
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"simple_image_cropper"
+            binaryMessenger:[registrar messenger]];
+  SimpleImageCropperPlugin* instance = [[SimpleImageCropperPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
 }
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
 @end
